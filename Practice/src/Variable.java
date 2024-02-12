ca40feab134cf23e516bf3a8ba6f4508bedeb31d
karthikdeeraj <71403717+karthikdeeraj@users.noreply.github.com>
2024-02-12 14:47:44 -0600

    Delete Practice directory

--- Practice/src/Variable.java
+++
@@ -1,9 +0,0 @@
-public class Variable {
-    int a = 3; // static variable;
-    public static void main(String[] args){
-        int b=5; // instance variable
-    }
-    void add(){
-        int c=7; // local variable
-    }
-}
